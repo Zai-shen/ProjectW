b7c0ecbb9efe8c3f5c4f37f45577c324ab44bb95
ConBoe <constantin.b@gmx.at>
2023-07-09 14:38:14 +0200

    something

--- Assets/PowderLevelHandler.cs
+++ Assets/PowderLevelHandler.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class PowderLevelHandler : MonoBehaviour
 {
-    public void displayFlour(){
-        Debug.Log(Globals.FlourPlayer);
+    public Button Button1;
+    public Button Button2;
+    public Button Button3;
+    public Text PowderLVLText;
+
+    private int tmpint;
+    public void OnLevelUp(){
+        // trigger when new level is reached
+        Globals.FlourNextLevel *= 2;
+        Globals.FlourLevel += 1;
+
+        int Rand;
+        int Lenght = 3;
+        int noptions = 7;
+        List<int> selection = new List<int>();
+        selection = new List<int>(new int[Lenght]);
+     
+            for (int j = 1; j < Lenght; j++)
+            {
+                Rand = Random.Range(0,noptions);
+     
+                while (selection.Contains(Rand))
+                {
+                    Rand = Random.Range(0,noptions);
+                }
+                selection[j] = Rand;
+                print(selection[j]);
+            }
     }
-
-
 }
